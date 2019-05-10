3ee9ba3dfc58a7892d25e8753af92b66e74a869c
Ryan Peacock <rpeacock@learn4life.org>
2019-05-10 10:14:51 -0700

    Updates

---
+++ precache-manifest.40d68493c99d5cedfdf5b6dfc5ea77ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b45ff319f8aad75d8e6fd8f640e753d",
+    "url": "/reactFrontBack/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/reactFrontBack/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d9387fc3c9d8a07687de",
+    "url": "/reactFrontBack/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/reactFrontBack/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "d9387fc3c9d8a07687de",
+    "url": "/reactFrontBack/static/js/main.aafd3282.chunk.js"
+  },
+  {
+    "revision": "13aec2ebd6a0a5905792",
+    "url": "/reactFrontBack/static/js/runtime~main.56e6ec78.js"
+  }
+]);
